54549e595ecf05b6883484c9ea0c525c56faa704
issacastro <issacastro11@gmail.com>
2020-12-04 21:36:30 -0600

    Version Beta

--- components/Forms/AudioForm.js
+++ components/Forms/AudioForm.js
@@ -84,7 +84,7 @@ export default function Form() {
       method: "POST",
       body: form,
     }); */
-    const res = await fetch("http://34.72.31.215:5000/upload", {
+    const res = await fetch("http://35.239.225.221:5000/upload", {
       mode: "cors",
       method: "POST",
       body: form,
